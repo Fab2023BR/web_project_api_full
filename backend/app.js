2601201fed768639ada99f44e4fbe9e4a2fcfd8e
Fabiana Storer <fabiana.storer@gmail.com>
2024-12-27 16:07:48 +0100

    hotfix

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import userRouter from './routes/users.js';
+import cardRouter from './routes/cards.js';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: "*"
+}));
+
+mongoose.connect('mongodb://localhost:27017/aroundb');
+
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'A solicitação não foi encontrada' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Aplicativo executando na porta ${PORT}`);
+});
---
+++ backend/controllers/users.js
@@ -0,0 +1,155 @@
+import User from "../models/user.js";
+import jwt from "jsonwebtoken";
+import { createHash } from "../utils/hash.js";
+
+export function getUsers(req, res) {
+  return User.find({})
+    .then((users) => {
+      if (!users) {
+        const err = new Error("Erro ao buscar usuários");
+        err.status = 500;
+        throw err;
+      }
+      res.send({ data: users });
+    })
+    .catch((err) => {
+      console.log("getUsers Error:", err);
+      res.status(err.status).send({ error: err.message });
+    });
+}
+
+export function getUserById(req, res) {
+  const { userId } = req.params;
+  return User.findById(userId)
+    .orFail(() => {
+      const err = new Error("Usuário não encontrado");
+      err.status = 404;
+      throw err;
+    })
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      console.log("getUserById Error:", err);
+      res.status(err.status).send({ error: err.message });
+    });
+}
+
+export function login(req, res) {
+  const { email, password } = req.body;
+
+  User.findUserByCredentials(email, password).then(({ user }) => {
+    if (!user) {
+      return res.status(401).json({
+        statusCode: 401,
+        message: "Email ou senha incorretos",
+      });
+    }
+    res.status(200).json({
+      userId: user._id,
+      token: jwt.sign(
+        { _id: user._id },
+        NODE_ENV === "production" ? JWT_SECRET : "dev-secret",
+        {
+          expiresIn: "7d",
+        }
+      ),
+    });
+  });
+}
+
+export function createUser(req, res) {
+  const { name, about, avatar, email, password } = req.body;
+  const hashedPassword = createHash(password);
+
+  if (!email || !password) {
+    return res.status(400).send({ error: "Dados inválidos..." });
+  }
+
+  return User.create({
+    name,
+    about,
+    avatar,
+    email,
+    password: hashedPassword,
+  })
+    .then((user) => {
+      if (!user) {
+        const err = new Error("Ocorreu um erro ao criar usuário");
+        err.status = 500;
+        throw err;
+      }
+      req.user._id = user._id;
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      console.log("createUser Error:", err);
+      res.status(err.status).send({ error: err.message });
+    });
+}
+
+export function updateUserProfile(req, res) {
+  const { name, about } = req.body;
+  console.log(name, about);
+  const userId = req.user._id;
+  const userUpdated = {};
+
+  if (name) {
+    userUpdated.name = name;
+  }
+  if (about) {
+    userUpdated.about = about;
+  }
+
+  if (!name && !about) {
+    return res.status(400).send({ error: "Dados inválidos..." });
+  }
+
+  return User.findByIdAndUpdate(userId, userUpdated, {
+    new: true,
+  })
+    .orFail(() => {
+      const err = new Error("Usuário não encontrado");
+      err.status = 404;
+      throw err;
+    })
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      console.log("updateUserProfile Error:", err);
+      res.status(err.status).send({ error: err.message });
+    });
+}
+
+export function updateUserAvatar(req, res) {
+  const { avatar } = req.body;
+  const userId = req.user._id;
+
+  if (!avatar) {
+    return res.status(400).send({ error: "Dados inválidos..." });
+  }
+
+  return User.findByIdAndUpdate(
+    userId,
+    {
+      $set: { avatar: avatar.avatar }, // Use $set para campos únicos
+    },
+    {
+      new: true,
+    }
+  )
+    .orFail(() => {
+      const err = new Error("Usuário não encontrado");
+      err.status = 404;
+      throw err;
+    })
+    .then((user) => {
+      console.log(user);
+      return res.send({ data: user });
+    })
+    .catch((err) => {
+      console.log("updateUserAvatar Error:", err);
+      return res.status(err.status).send({ error: err.message });
+    });
+}
